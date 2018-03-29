c40fb467292709591a1b0fc1042708d348fbf28a
Alexander Londoño Espejo <alexlondon07@gmail.com>
2018-03-28 21:16:44 -0500

    Refactoring

--- app/src/main/java/io/github/alexlondon07/emistore/client/presenter/ClientPresenter.java
+++ app/src/main/java/io/github/alexlondon07/emistore/client/presenter/ClientPresenter.java
@@ -1,5 +1,11 @@
 package io.github.alexlondon07.emistore.client.presenter;
 
+import java.util.ArrayList;
+
+import io.github.alexlondon07.emistore.R;
+import io.github.alexlondon07.emistore.client.model.Client;
+import io.github.alexlondon07.emistore.client.repository.IClientRepository;
+import io.github.alexlondon07.emistore.client.repository.RepositoryError;
 import io.github.alexlondon07.emistore.client.view.IClientView;
 import io.github.alexlondon07.emistore.util.BasePresenter;
 
@@ -9,5 +15,45 @@ import io.github.alexlondon07.emistore.util.BasePresenter;
 
 public class ClientPresenter extends BasePresenter<IClientView> {
 
+    private IClientRepository clientRepository;
 
+    public ClientPresenter(IClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+
+    public void getClientsPresenter(){
+        if(getValidateInternet().isConnected()){
+            createThreadClient();
+        }else{
+            getView().showAlertDialog(R.string.error, R.string.validate_internet);
+        }
+    }
+
+    private void createThreadClient() {
+        getView().showProgress(R.string.loading_message);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                getClientsRepository();
+            }
+        });
+        thread.start();
+    }
+
+    /**
+     * Función para obtener listado de Cliente de bd
+     */
+    private void getClientsRepository() {
+        try {
+
+            ArrayList<Client>  clientArrayList = clientRepository.getClients();
+            getView().showClientList(clientArrayList);
+
+        }catch (RepositoryError repositoryError){
+            getView().showAlertDialog(R.string.error, repositoryError.toString());
+        }finally {
+            getView().hideProgress();
+        }
+    }
 }
